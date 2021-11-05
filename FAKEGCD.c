499a949b28c5b405e22e20d7d70de30cced4c372
toxic-coderr <abhudayaaman@gmail.com>
2021-11-05 12:14:39 +0530

    COOK133C

---
+++ FAKEGCD.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,t,j;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        scanf("%d",&n);
+        
+        for(j=1;j<=n;j++)
+        {
+         printf("%d ",j);
+        
+        }
+    }
+   
+
+    return 0;
+}
