f2831411c713d25b44418d871b7725a29d3cb236
toxic-coderr <abhudayaaman@gmail.com>
2021-11-26 15:01:19 +0530

    CCSTART12

---
+++ VALTRI.c
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int t;
+    cin>>t;
+    if(t%5==0 || t%6==0)
+     cout<<"YES";
+     else
+     cout<<"NO";
+    
+}
+
+
+int main() 
+{
+
+	    solve();
+	
+	return 0;
+}
