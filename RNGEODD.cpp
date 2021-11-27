5b61081577866c36d121d5c8eda0e3f19e0e3908
toxic-coderr <abhudayaaman@gmail.com>
2021-11-27 21:27:36 +0530

    CCSTART2

---
+++ RNGEODD.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int l,r,i;
+    cin>>l>>r;
+    for(i=l;i<=r;i++)
+    {
+        if(i%2!=0)
+        cout<<i<<" ";
+    }
+    
+    
+}
+
+int main() 
+{
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+	    solve();
+// 	}
+	return 0;
+}
