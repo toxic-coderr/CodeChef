c644f7f2027696a450e9da260a877ac9b39c8f94
toxic-coderr <abhudayaaman@gmail.com>
2021-12-05 19:43:33 +0530

    CCSTART2

---
+++ ADDNATRL.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long int n,i,sum=0,p=0;
+    cin>>n;
+    // p=pow(10,9);
+    for(i=1;i<=n;i++)
+    {
+        sum+=i;
+        
+    }
+    cout<<sum;
+}
+
+
+int main() 
+{
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+	    solve();
+// 	}
+	return 0;
+}
