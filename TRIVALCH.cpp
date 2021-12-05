c14a13a73a16668f990d4e691655a6428bab4ff5
toxic-coderr <abhudayaaman@gmail.com>
2021-12-06 00:07:39 +0530

    CCSTART2

---
+++ TRIVALCH.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<cmath>
+using namespace std;
+
+void solve()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+   if(a+b>c && b+c>a && c+a>b)
+   cout<<"YES\n";
+   else
+   cout<<"NO\n";
+    
+}
+
+
+int main() 
+{
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+	    solve();
+// 	}
+	return 0;
+}
