418e821d17165c30e1d758ef61aae78548b17000
toxic-coderr <abhudayaaman@gmail.com>
2021-12-07 19:29:36 +0530

    CCSTART2

---
+++ SUMEVOD.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+  long long int n,o,e;
+   cin>>n;
+   o=n*n;
+   e=n*(n+1);
+   cout<<o<<" "<<e;
+}
+
+
+int main() 
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+	    solve();
+// 	}
+	return 0;
+}
